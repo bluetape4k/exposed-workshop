6f10bcce00df8b58882d01dacea6446f2f20a32b
Debop <sunghyouk.bae@gmail.com>
2025-02-24 17:32:48 +0900

    feat: Add DistinctOn example

--- 05-exposed-dml/01-exposed-dml/src/test/kotlin/exposed/dml/example/Ex07_DistinctOn.kt
+++ 05-exposed-dml/01-exposed-dml/src/test/kotlin/exposed/dml/example/Ex07_DistinctOn.kt
@@ -2,12 +2,16 @@ package exposed.dml.example
 
 import exposed.shared.tests.AbstractExposedTest
 import exposed.shared.tests.TestDB
+import exposed.shared.tests.expectException
 import exposed.shared.tests.withTables
 import io.bluetape4k.logging.KLogging
 import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldBeNull
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.SortOrder.ASC
 import org.jetbrains.exposed.sql.batchInsert
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.params.ParameterizedTest
@@ -144,6 +148,120 @@ class Ex07_DistinctOn: AbstractExposedTest() {
                 .map { it[tester.v1] to it[tester.v2] }
 
             distinctSequential shouldBeEqualTo distinctBoth
+        }
+    }
+
+
+    /**
+     * `withDistinct` 와 `withDistinctOn` 을 동시에 사용하면 예외가 발생합니다.
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `exception when distinct and distinctOn`(testDB: TestDB) {
+        Assumptions.assumeTrue { testDB in distinctOnSupportedDb }
+
+        /**
+         * ```sql
+         * -- Postgres
+         * CREATE TABLE IF NOT EXISTS tester (
+         *      id SERIAL PRIMARY KEY,
+         *      v1 INT NOT NULL
+         * )
+         * ```
+         */
+        val tester = object: IntIdTable("tester") {
+            val v1 = integer("v1")
+        }
+
+        withTables(testDB, tester) {
+            val query1 = tester.selectAll().withDistinct()
+            expectException<IllegalArgumentException> {
+                query1.withDistinctOn(tester.v1)
+            }
+
+            val query2 = tester.selectAll().withDistinctOn(tester.v1)
+            expectException<IllegalArgumentException> {
+                query2.withDistinct()
+            }
+        }
+    }
+
+
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `empty distinctOn`(testDB: TestDB) {
+        Assumptions.assumeTrue { testDB in distinctOnSupportedDb }
+
+        /**
+         * Postgres:
+         * ```sql
+         * CREATE TABLE IF NOT EXISTS tester (
+         *      id SERIAL PRIMARY KEY,
+         *      v1 INT NOT NULL
+         * )
+         * ```
+         */
+        val tester = object: IntIdTable("tester") {
+            val v1 = integer("v1")
+        }
+
+        withTables(testDB, tester) {
+            tester.insert {
+                it[tester.v1] = 1
+            }
+
+            // `withDistinctOn` 에 컬럼을 지정하지 않아도 예외가 발생하지 않습니다.
+            val query = tester.selectAll()
+                .withDistinctOn(columns = emptyArray<Column<*>>())
+
+            query.distinctOn.shouldBeNull()
+
+            /**
+             * 컬럼을 제공하지 않았으므로, `distinct on` 이 사용되지 않는다.
+             *
+             * Postgres:
+             * ```sql
+             * SELECT tester.id, tester.v1
+             *   FROM tester
+             * ```
+             */
+            val value = query.first()[tester.v1]
+            value shouldBeEqualTo 1
+        }
+    }
+
+    /**
+     * `withDistinctOn` 을 사용한 쿼리에 대한 COUNT 쿼리를 실행합니다. (MySQL에서는 group by 를 사용해야 합니다.)
+     *
+     * ```sql
+     * -- Postgres
+     * SELECT COUNT(*)
+     *   FROM (SELECT DISTINCT ON (tester."name")
+     *                tester."name"
+     *                tester_name
+     *           FROM tester
+     *        ) subquery
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `distinctOn with count`(testDB: TestDB) {
+        Assumptions.assumeTrue { testDB in distinctOnSupportedDb }
+
+        val tester = object: IntIdTable("tester") {
+            val name = varchar("name", 50)
+        }
+        withTables(testDB, tester) {
+            val names = listOf("a", "a", "b", "c", "b")
+            tester.batchInsert(names) {
+                this[tester.name] = it
+            }
+
+            val count = tester.select(tester.name)
+                .withDistinctOn(tester.name)
+                .count()
+
+            count shouldBeEqualTo names.distinct().size.toLong()  // 3L
         }
     }
 }
