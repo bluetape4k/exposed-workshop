097f93ae7d15b27022c949a90becc1e82922db05
Debop <sunghyouk.bae@gmail.com>
2025-02-25 23:27:31 +0900

    feat: ExposedBlob 필드를 가진 엔티티에 대한 예제

---
+++ 05-exposed-dml/02-entities/src/test/kotlin/exposed/examples/entities/Ex06_EntityWithBlob.kt
@@ -0,0 +1,103 @@
+package exposed.examples.entities
+
+import exposed.shared.tests.AbstractExposedTest
+import exposed.shared.tests.TestDB
+import exposed.shared.tests.withTables
+import io.bluetape4k.exposed.dao.idEquals
+import io.bluetape4k.exposed.dao.idHashCode
+import io.bluetape4k.exposed.dao.toStringBuilder
+import io.bluetape4k.exposed.sql.statements.api.toUtf8String
+import io.bluetape4k.junit5.faker.Fakers
+import io.bluetape4k.logging.KLogging
+import io.bluetape4k.support.toUtf8Bytes
+import io.bluetape4k.support.toUtf8String
+import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldBeNull
+import org.jetbrains.exposed.dao.LongEntity
+import org.jetbrains.exposed.dao.LongEntityClass
+import org.jetbrains.exposed.dao.entityCache
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.statements.api.ExposedBlob
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+/**
+ * [ExposedBlob] 필드를 가지는 엔티티를 다루는 예제입니다.
+ */
+class Ex06_EntityWithBlob: AbstractExposedTest() {
+
+    companion object: KLogging()
+
+    /**
+     * ```sql
+     * -- Postgres
+     * CREATE TABLE IF NOT EXISTS blobtable (
+     *      id BIGSERIAL PRIMARY KEY,
+     *      "content" bytea NULL
+     * )
+     * ```
+     */
+    object BlobTable: LongIdTable("BlobTable") {
+        val blob = blob("content").nullable()
+    }
+
+    class BlobEntity(id: EntityID<Long>): LongEntity(id) {
+        companion object: LongEntityClass<BlobEntity>(BlobTable) {
+            /**
+             * Custom 생성자
+             */
+            operator fun invoke(bytes: ByteArray): BlobEntity {
+                return new { content = ExposedBlob(bytes) }
+            }
+        }
+
+        var content: ExposedBlob? by BlobTable.blob
+
+        override fun equals(other: Any?): Boolean = idEquals(other)
+        override fun hashCode(): Int = idHashCode()
+        override fun toString(): String = toStringBuilder()
+            .add("content", content?.bytes?.toUtf8String())
+            .toString()
+    }
+
+    /**
+     * Blob 필드를 다루는 테스트
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `handle blob field`(testDB: TestDB) {
+        withTables(testDB, BlobTable) {
+
+            val text1 = Fakers.randomString(1024, 4096)
+            val bytes1 = text1.toUtf8Bytes()
+
+            // INSERT INTO blobtable ("content") VALUES (E'\\x')
+            val blobEntity = BlobEntity(bytes1)
+            entityCache.clear()
+
+            // SELECT blobtable.id, blobtable."content" FROM blobtable WHERE blobtable.id = 1
+            var y2 = BlobEntity.reload(blobEntity)!!
+            y2.content!!.bytes.toUtf8String() shouldBeEqualTo text1
+
+            // UPDATE blobtable SET "content"=NULL WHERE id = 1
+            y2.content = null
+            entityCache.clear()
+
+            // SELECT blobtable.id, blobtable."content" FROM blobtable WHERE blobtable.id = 1
+            y2 = BlobEntity.reload(blobEntity)!!
+            y2.content.shouldBeNull()
+
+            val text2 = Fakers.randomString(1024, 4096)
+            val bytes2 = text2.toUtf8Bytes()
+
+            // UPDATE blobtable SET "content"=E'\\x' WHERE id = 1
+            y2.content = ExposedBlob(bytes2)
+            entityCache.clear()
+
+            // SELECT blobtable.id, blobtable."content" FROM blobtable WHERE blobtable.id = 1
+            y2 = BlobEntity.reload(blobEntity)!!
+            y2.content!!.toUtf8String() shouldBeEqualTo text2
+        }
+    }
+}
