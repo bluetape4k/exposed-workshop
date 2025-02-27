847b6368e03534be19a956f6b0fbc865de3fcc5a
Debop <sunghyouk.bae@gmail.com>
2025-02-27 18:42:23 +0900

    feat: TransactionManager 예제 추가

---
+++ 05-exposed-dml/05-transactions/src/test/kotlin/exposed/examples/transactions/TransactionTables.kt
@@ -0,0 +1,21 @@
+package exposed.examples.transactions
+
+import exposed.shared.tests.TestDB
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+/**
+ * ```sql
+ * CREATE TABLE IF NOT EXISTS rollbacktable (
+ *      id SERIAL PRIMARY KEY,
+ *      "value" VARCHAR(20) NOT NULL
+ * );
+ * ```
+ */
+object RollbackTable: IntIdTable("rollbackTable") {
+    val value = varchar("value", 20)
+}
+
+// Explanation: MariaDB driver never set readonly to true, MSSQL silently ignores the call, SQLite does not
+// promise anything, H2 has very limited functionality
+val READ_ONLY_EXCLUDED_VENDORS = TestDB.ALL_H2 + TestDB.ALL_MARIADB
+// + TestDB.ALL_MARIADB + listOf(TestDB.SQLITE, TestDB.SQLSERVER, TestDB.ORACLE)
