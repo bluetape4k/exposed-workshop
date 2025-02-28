813b586455539f37abcacc95e0e27eb842ecda89
Debop <sunghyouk.bae@gmail.com>
2025-02-28 22:35:51 +0900

    feat: 테이블의 PK를 클라이언트에서 Timebased UUID를 Base62로 인코딩한 문자열로 생성하는 Table 정의

---
+++ 06-advanced/07-custom-entities/src/main/kotlin/exposed/examples/custom/entities/TimebasedUUIDBase62Table.kt
@@ -0,0 +1,34 @@
+package exposed.examples.custom.entities
+
+import io.bluetape4k.idgenerators.uuid.TimebasedUuid
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.EntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IdTable
+
+/**
+ * 테이블의 `id` 컬럼을 클라이언트에서 생성한 Timebased UUID 값을 Base62로 인코딩한 값을 사용하는 Table
+ */
+open class TimebasedUUIDBase62Table(
+    name: String = "",
+    columnName: String = "id",
+): IdTable<String>(name) {
+    final override val id = varchar(columnName, 22)
+        .clientDefault { TimebasedUuid.Reordered.nextIdAsString() }
+        .entityId()
+
+    final override val primaryKey = PrimaryKey(id)
+}
+
+typealias TimebasedUUIDBase62EntityID = EntityID<String>
+
+/**
+ * 엔티티의 `id` 속성을 클라이언트에서 생성한 Timebased UUID 값을 사용하는 Entity
+ */
+open class TimebasedUUIDBase62Entity(id: TimebasedUUIDBase62EntityID): Entity<String>(id)
+
+open class TimebasedUUIDBase62EntityClass<out E: TimebasedUUIDBase62Entity>(
+    table: TimebasedUUIDBase62Table,
+    entityType: Class<E>? = null,
+    entityCtor: ((TimebasedUUIDBase62EntityID) -> E)? = null,
+): EntityClass<String, E>(table, entityType, entityCtor)
