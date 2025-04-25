90065ad2995de407f9d60452617867efcb34537b
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-04-25 11:33:56 +0900

    feat: 11-high-performance 주제로 예제 추가
    1. cache strategies 예제 제공
    2. routing datasource 구조화 예제
    
    Signed-off-by: Sunghyouk Bae <sunghyouk.bae@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,6 +26,7 @@ includeModules("07-jpa", false, false)
 includeModules("08-coroutines", false, false)
 includeModules("09-spring", false, false)
 includeModules("10-multi-tenant", false, false)
+includeModules("11-high-performance", false, false)
 
 fun includeModules(baseDir: String, withProjectName: Boolean = true, withBaseDir: Boolean = true) {
     files("$rootDir/$baseDir").files
