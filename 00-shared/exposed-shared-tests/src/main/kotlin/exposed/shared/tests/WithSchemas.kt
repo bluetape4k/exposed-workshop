63e7643472f70f997ebc23ca36fb423a5151e73b
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-05-01 13:47:03 +0900

    feat: 데이터베이스 및 스키마 유틸 메서드 개선
    - withDb 및 withSuspendedDb 함수에서 configure의 기본값을 null에서 빈 람다로 변경
    - withTables 및 withSuspendedTables 함수에 dropTables 옵션 추가
    - withSchemas 및 withSuspendedSchemas 함수 개선 및 withSuspendedSchemas 함수 추가
      - 스키마 생성 및 삭제 로직 추가
      - 커밋 후 스키마 삭제 처리 로직 도입
    - 예외 처리 강화 및 불필요한 설정 유지 방지를 위한 finally 블록 추가
    
    Signed-off-by: Sunghyouk Bae <sunghyouk.bae@gmail.com>

--- 00-shared/exposed-shared-tests/src/main/kotlin/exposed/shared/tests/WithSchemas.kt
+++ 00-shared/exposed-shared-tests/src/main/kotlin/exposed/shared/tests/WithSchemas.kt
@@ -1,17 +1,41 @@
 package exposed.shared.tests
 
+import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.DatabaseConfig
 import org.jetbrains.exposed.sql.Schema
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Transaction
+import kotlin.coroutines.CoroutineContext
 
 fun withSchemas(
     dialect: TestDB,
     vararg schemas: Schema,
-    configure: (DatabaseConfig.Builder.() -> Unit)? = null,
+    configure: (DatabaseConfig.Builder.() -> Unit)? = {},
     statement: Transaction.() -> Unit,
 ) {
     withDb(dialect, configure) {
+        if (currentDialectTest.supportsCreateSchema) {
+            SchemaUtils.createSchema(*schemas)
+            try {
+                statement()
+                commit()     // Need commit to persist data before drop schemas
+            } finally {
+                SchemaUtils.dropSchema(*schemas, cascade = true)
+                commit()
+            }
+        }
+    }
+}
+
+
+suspend fun withSuspendedSchemas(
+    dialect: TestDB,
+    vararg schemas: Schema,
+    configure: (DatabaseConfig.Builder.() -> Unit)? = {},
+    context: CoroutineContext? = Dispatchers.IO,
+    statement: Transaction.() -> Unit,
+) {
+    withSuspendedDb(dialect, configure = configure, context = context) {
         if (currentDialectTest.supportsCreateSchema) {
             SchemaUtils.createSchema(*schemas)
             try {
