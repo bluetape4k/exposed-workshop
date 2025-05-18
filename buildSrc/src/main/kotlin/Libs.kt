cec392411d07cf9c95b526cab5bfdb8010193048
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-05-19 01:13:01 +0900

    chore: Kotlin 버전 업데이트 (2.1.20 → 2.1.21)
    Signed-off-by: Sunghyouk Bae <sunghyouk.bae@gmail.com>

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -63,7 +63,7 @@ object Versions {
     // Java 21, Kotlin 2.0 이상에서 사용하세요
     const val bluetape4k = "0.0.6-SNAPSHOT"              // Bluetape4k Github Package 로 배포됩니다.
 
-    const val kotlin = "2.1.20"                 // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
+    const val kotlin = "2.1.21"                 // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
     const val kotlinx_coroutines = "1.10.2"      // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
     const val kotlinx_serialization = "1.8.1"   // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-serialization-json-jvm
 
