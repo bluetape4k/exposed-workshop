eae2a7da3cf322634e5dcf228d54fff8d15c75e2
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-09-14 21:56:27 +0900

    build(deps): Jackson 라이브러리 버전 2.20.0으로 업데이트
    Signed-off-by: Sunghyouk Bae <sunghyouk.bae@gmail.com>

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -116,7 +116,7 @@ object Versions {
     const val okio = "3.16.0"         // https://mvnrepository.com/artifact/com.squareup.okio/okio
     const val asynchttpclient = "2.12.4"  // https://mvnrepository.com/artifact/org.asynchttpclient/async-http-client
 
-    const val jackson = "2.19.2" // https://mvnrepository.com/artifact/com.fasterxml.jackson/jackson-bom
+    const val jackson = "2.20.0" // https://mvnrepository.com/artifact/com.fasterxml.jackson/jackson-bom
     const val fastjson2 = "2.0.58" // https://mvnrepository.com/artifact/com.alibaba.fastjson2/fastjson2
     const val jjwt = "0.11.5"    // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt-api
 
