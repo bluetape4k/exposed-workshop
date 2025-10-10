7cc4b621b7182ef0c2bd4570885587280cdae1fc
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-10-10 23:07:44 +0900

    refactor: 라이브러리 버전 업데이트
    - exposed: 1.0.0-rc-1 -> 1.0.0-rc-2
    
    Signed-off-by: Sunghyouk Bae <sunghyouk.bae@gmail.com>

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -136,7 +136,7 @@ object Versions {
     const val hibernate_validator = "8.0.3.Final" // https://mvnrepository.com/artifact/org.hibernate.validator/hibernate-validator
     const val querydsl = "5.1.0"                  // https://mvnrepository.com/artifact/com.querydsl/querydsl-jpa
 
-    const val exposed = "1.0.0-rc-1"       // https://mvnrepository.com/artifact/org.jetbrains.exposed/exposed-core
+    const val exposed = "1.0.0-rc-2"       // https://mvnrepository.com/artifact/org.jetbrains.exposed/exposed-core
 
     const val r2dbc = "1.0.0.RELEASE"        // https://mvnrepository.com/artifact/io.r2dbc/r2dbc-spi
 
